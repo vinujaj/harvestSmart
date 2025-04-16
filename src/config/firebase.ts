64302e58e45d2de5d516bfd94431b6fe69628263
Vinuja Jayathilake <vinujajayathilake@gmail.com>
2025-04-16 07:30:13 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,7 +6,6 @@ const firebaseConfig = {
   projectId: "harvestsmart-48704",
   appId: "1:707509188885:android:264a12c74cdd68b52fd11c",
   storageBucket: "harvestsmart-48704.appspot.com",
-  apiKey: "AIzaSyBGWODXkXF2qm_Z4P5-0X5ZF9kFWzgkYHM",
   authDomain: "harvestsmart-48704.firebaseapp.com",
   messagingSenderId: "707509188885",
   databaseURL: "https://harvestsmart-48704.firebaseio.com",
